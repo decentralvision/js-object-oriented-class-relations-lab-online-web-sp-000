4e14f885790dbe3fc1dba5b4c869afacd819bd40
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-07-09 05:15:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,36 @@ class Driver {
 class Passenger {
   constructor(name) {
     this.name = name
-    this.id
+    this.id = passengerId++
+    store.passengers.push(this)
+  }
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.passengerId == this.id
+    })
+  }
+  drivers() {
+    return this.trips().map(trip => {
+      return trip.driver()
+    })
+  }
+}
+
+class Trip {
+  constructor(driver, passenger) {
+    this.driverId = driver.id
+    this.passengerId = passenger.id
+    this.id = tripId++
+    store.trips.push(this)
+  }
+  driver() {
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId
+    })
+  }
+  passenger() {
+    return store.passengers.gind(passenger => {
+      return passenger.id === this.passengerId
+    })
   }
 }
